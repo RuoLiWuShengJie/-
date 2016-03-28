6168a3583e8abbe7202b16debf36bf4b644997e5
RuoLiWuShengJie <469912605@qq.com@wushengjie.local>
2016-03-28 15:00:11 +0800

    1465
    13

---
+++ 录音/customTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  customTableViewCell.h
+//  录音
+//
+//  Created by 吴胜杰 on 16/3/28.
+//  Copyright © 2016年 DPSoftwareDevelopmentCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface customTableViewCell : UITableViewCell
+
+@end
---
+++ 录音/customTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  customTableViewCell.m
+//  录音
+//
+//  Created by 吴胜杰 on 16/3/28.
+//  Copyright © 2016年 DPSoftwareDevelopmentCompany. All rights reserved.
+//
+
+#import "customTableViewCell.h"
+
+@implementation customTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
